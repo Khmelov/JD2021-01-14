5333d12f2673245d96e04aa948042580a242df04
it.academy.vs@gmail.com <it.academy.vs@gmail.com>
2021-01-26 14:08:25 +0300

    classwork jd01_06

---
+++ src/by/it/khmelov/jd01_02/TaskA.java
@@ -0,0 +1,65 @@
+package by.it.khmelov.jd01_02;
+
+
+import java.util.Scanner;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        step1(array);
+        step2(array);
+        step3(array);
+    }
+
+
+    private static void step1(int[] array) {
+        int min = array[0];
+        int max = array[0];
+        for (int value : array) {
+            if (value > max) {
+                max = value;
+            }
+            if (value < min) {
+                min = value;
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+
+    private static void step2(int[] array) {
+        int sum = 0;
+        for (int value : array) {
+            sum += value;
+        }
+        double avg = (double) sum / array.length;
+        for (int value : array) {
+            if (value < avg) {
+                System.out.print(value + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void step3(int[] array) {
+        int min = array[0];
+        for (int value : array) {
+            if (value < min) {
+                min = value;
+            }
+        }
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (min == array[i]) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+
+}
---
+++ src/by/it/khmelov/jd01_06/TaskA2.java
@@ -0,0 +1,37 @@
+package by.it.khmelov.jd01_06;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskA2 {
+
+    private static String[] words = new String[0];
+    private static int[] counts = {};
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            String word = matcher.group();
+            process(word);
+        }
+        for (int i = 0; i < words.length; i++) {
+            System.out.printf("%s=%d\n",words[i],counts[i]);
+        }
+    }
+
+    private static void process(String word) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word)) {
+                counts[i]++;
+                return;
+            }
+        }
+        words = Arrays.copyOf(words, words.length + 1);
+        words[words.length - 1] = word;
+        counts = Arrays.copyOf(counts, counts.length + 1);
+        counts[counts.length - 1] = 1;
+    }
+}
+
