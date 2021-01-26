71e4eb67f0a3f96f753ccd61dede07a4193d264b
aliaksei <alekseinakhankov@gmail.com>
2021-01-26 09:39:34 +0300

    jd01_05

---
+++ src/by/it/nakhankov/jd01_05/TaskB.java
@@ -0,0 +1,47 @@
+package by.it.nakhankov.jd01_05;
+
+import static java.lang.Math.*;
+
+public class TaskB {
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    private static void task1() {
+
+        for (double a = 0.0; a <= 2; a = a + 0.2) {
+            double sum = 0;
+
+            for (int j = 1; j < 7; j++) {
+                sum = sum + pow(7, a) - cos(j);
+            }
+            System.out.printf("При a=%-9.2fСумма y=%e\n", a, sum);
+        }
+        System.out.println();
+    }
+
+    static void task2() {
+        double divider = 2.0;
+
+        for (double x = -6; x < 2; x = x + 0.5) {
+            if (x > -6) {
+                if (-2 < x / divider && x / divider <= -1) {
+                    printTask2(x / divider, log10(abs(sin(pow(x, 2)) + 2.74)));
+                } else if (-1 < x / divider && x / divider < 0.2) {
+                    printTask2(x / divider, log10(abs(cos(pow(x, 2)) + 2.74)));
+                } else if (x / divider == 0.2) {
+                    printTask2(x / divider, log10(abs(cos(pow(x, 2)) / sin(pow(x, 2)) + 2.74)));
+                } else {
+                    System.out.printf("При x/2 = %-9.2f a = %s\n", x / divider, "вычисления не определены");
+                }
+            }
+        }
+    }
+
+
+    static void printTask2(double magnitude, double number) {
+        System.out.printf("При x/2 = %-9.2f a = %e\n", magnitude, number);
+    }
+}
