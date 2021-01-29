34cdfcdb69e2379dad7fd19ebdd06ba1e3c54eb9
Zevalich <afina.vi.s@gmail.com>
2021-01-30 01:35:32 +0300

    ..

--- src/by/it/zevalich/jd01_06/TaskB1.java
+++ src/by/it/zevalich/jd01_06/TaskB1.java
@@ -1,8 +1,9 @@
 package by.it.zevalich.jd01_06;
 
-
 public class TaskB1 {
+
     static String[] strArray = Poem.text.split("\\s*(\\s|,|!|\\.)\\s*");
-    
+    static char[] vowels = new char[]{'а', 'о', 'у', 'э', 'ы', 'я', 'ё', 'ю', 'е', 'и'};
+    static char[] consonant = new char[]{'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'щ', 'ш'};
 
 }
