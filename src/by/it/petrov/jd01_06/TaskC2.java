fb55f10a28a21096c7ee95cbdcf8e77aa012fd64
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-01-30 09:34:48 +0300

    jd05

--- src/by/it/petrov/jd01_06/TaskC2.java
+++ src/by/it/petrov/jd01_06/TaskC2.java
@@ -3,8 +3,8 @@ package by.it.petrov.jd01_06;
 import java.util.Arrays;
 
 public class TaskC2 {
-    static String slow (String text){
-
+    static String[] randomWordsArray;
+    static String[] getSequence (String text){
         String formattedText = text.replace("\n", " ");
         formattedText = formattedText.replace(".", " ");
         formattedText = formattedText.replace(",", " ");
@@ -22,9 +22,13 @@ public class TaskC2 {
         for (int i = 0; i < 100000; i++) {
             randomWordsArray[i] = wordsArray[randomNumberInGivenRangeArray[i]];
         }
+        return randomWordsArray;
+    }
+    static String slow (String text){
+
         String finalTextIs = "";
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < randomWordsArray.length; i++) {
             finalTextIs = finalTextIs + " " + randomWordsArray[i];
         }
 
@@ -32,39 +36,24 @@ public class TaskC2 {
     }
     static String fast (String text){
 
-        String formattedText = text.replace("\n", " ");
-        formattedText = formattedText.replace(".", " ");
-        formattedText = formattedText.replace(",", " ");
-        formattedText = formattedText.replace(":", " ");
-        formattedText = formattedText.replace("-", " ");
-        formattedText = formattedText.replace("!", " ");
-
-        String[] wordsArray = formattedText.split("[\\p{Blank}]+");
-        int[] randomNumberInGivenRangeArray = new int[100000];
-        for (int i = 0; i < 100000; i++){
-            randomNumberInGivenRangeArray[i] = (int)(Math.random() * wordsArray.length);
-        }
-
-        String[] randomWordsArray = new String[100000];
-        for (int i = 0; i < 100000; i++) {
-            randomWordsArray[i] = wordsArray[randomNumberInGivenRangeArray[i]];
-        }
         StringBuilder finalTextIs = new StringBuilder();
 
-        for (int i = 0; i < 100000; i++) {
-            finalTextIs = finalTextIs.append(" ");
+        for (int i = 0; i < randomWordsArray.length; i++){
+            finalTextIs = finalTextIs.append(randomWordsArray[i]).append(" ");
         }
         return finalTextIs.toString();
     }
+
     public static void main(String[] args) {
 
+        randomWordsArray = getSequence(Poem.text);
         long timeCounter1 = System.nanoTime()/ 10000000;
-        String text = slow(Poem.text);
+        String text = slow(" ");
         long timeCounter2 = System.nanoTime()/ 10000000;
         System.out.println(timeCounter2 - timeCounter1);
 
         long timeCounter3 = System.nanoTime()/ 10000000;
-        String text2 = fast(Poem.text);
+        String text2 = fast(" ");
         System.out.println(text);
         long timeCounter4 = System.nanoTime() / 10000000;
         System.out.println(timeCounter4 - timeCounter3);
