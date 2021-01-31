2e64ea5a6ac6258001675785834be7282a339bb6
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-01-31 23:16:57 +0300

    jd05

---
+++ src/by/it/petrov/jd01_08/Matrix.java
@@ -0,0 +1,145 @@
+package by.it.petrov.jd01_08;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Matrix extends Var {
+
+    private final double[][] value;
+
+    public Matrix(double[][] value) {
+        this.value = value;
+    }
+
+    public Matrix(Matrix matrix) {
+        this.value = matrix.value;
+    }
+
+    public Matrix(String matrixStr){
+        int elements = 0;
+        int columns = 0;
+        int rows = 0;
+
+        Pattern patternForDoubleNumbers = Pattern.compile("[0-9.]+");
+        Pattern patternForCurlyBraces = Pattern.compile("[}]");
+        Matcher matcherForDoubleNumbers = patternForDoubleNumbers.matcher(matrixStr);
+        Matcher matcherForCurlyBraces = patternForCurlyBraces.matcher(matrixStr);
+        int indexOfFirstCurlyBraces = 0;
+        if (matcherForCurlyBraces.find()){
+            indexOfFirstCurlyBraces = matcherForCurlyBraces.start();
+        }
+        while(matcherForDoubleNumbers.find()){
+            if (matcherForDoubleNumbers.start() > matcherForCurlyBraces.start()){
+                elements += 1;
+                continue;
+            }
+            columns += 1;
+            elements += 1;
+        }
+        rows = elements / columns;
+        double[][] matrix = new double[rows][columns];
+        String [] stringArray = matrixStr.split("[, }]+");
+        for (int i = 0; i < stringArray.length; i++) {
+            stringArray[i] = stringArray[i].replace("{{", "").replace("{", "");
+        }
+        for (int i = 0, k = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = Double.parseDouble(stringArray[k]);
+                k++;
+            }
+        }
+        this.value = matrix;
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof  Scalar){
+            double[][] result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                for (int j = 0; j < result[0].length; j++) {
+                    result[i][j] += ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(result);
+        }
+        if (other instanceof Matrix){
+            double[][] result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                for (int j = 0; j < result[0].length; j++) {
+                    result[i][j] += ((Matrix) other).value[i][j];
+                }
+            }
+            return new Matrix(result);
+        }
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Matrix){
+            double[][]result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                for (int j = 0; j < result[0].length; j++) {
+                    result[i][j] -= ((Matrix) other).value[i][j];
+                }
+            }
+            return new Matrix(result);
+        }
+        if (other instanceof Scalar){
+            double[][]result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                for (int j = 0; j < result[0].length; j++) {
+                    result[i][j] -= ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(result);
+        }
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar){
+            double result = 0;
+            for (int i = 0; i < value.length; i++) {
+                for (int j = 0; j < value[0].length; j++) {
+                    result += value[i][j]*((Scalar) other).getValue();
+                }
+            }
+            return new Scalar(result);
+        }
+        if(other instanceof Matrix){
+            double[][] result = new double[value.length][((Matrix) other).value[0].length];
+            for (int i = 0; i < value.length; i++) {
+                for (int j = 0; j < ((Matrix) other).value[0].length; j++) {
+                    for (int k = 0; k < ((Matrix) other).value.length; k++) {
+                        result[i][j] += value[i][k] * ((Matrix) other).value[k][j];
+                    }
+                }
+            }
+            return new Matrix(result);
+        }
+        return super.mul(other);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder("{{");
+        for (int i = 0; i < value.length; i++) {
+            for (int j = 0; j < value[0].length; j++) {
+                if (j == value[0].length - 1){
+                    sb = sb.append((value[i][j]));
+                    if(i == value.length - 1){
+                        sb = sb.append("}}");
+                        break;
+                    }
+                    sb = sb.append("}, {");
+                    continue;
+                }
+                sb = sb.append(value[i][j]).append(", ");
+            }
+        }
+        return sb.toString();
+    }
+}
