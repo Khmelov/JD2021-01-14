0aa4caa7fa249200b53c499b2224e186dc2fbac4
vesniachok <witharinavesna@gmail.com>
2021-02-01 19:28:30 +0300

    jd

--- src/by/it/vesniachok/jd01_04/Helper.java
+++ src/by/it/vesniachok/jd01_04/Helper.java
@@ -1,18 +1,59 @@
 package by.it.vesniachok.jd01_04;
 
 public class Helper {
-    static void sort(double[] a){
-        for (int last = a.length -1; last>1;last--)  {
-            for (int i = 0; i < last; i++) {
-                if(a[i]>a[i+1]){
-                    double buf = a[i];
-                    a[i] = a[i+1];
-                    a[i+1]=buf;
-
+    static double findMax(double[] arr) {
+        if (arr.length == 0) {
+            return Integer.MAX_VALUE;
+        } else {
+            double max = arr[0];
+            for (double i : arr) {
+                if (max < i) {
+                    max = i;
                 }
-
             }
+            return max;
+        }
+
+    }
+
+    static double findMin(double[] arr) {
+        if (arr.length == 0) {
+            return Integer.MIN_VALUE;
+        } else {
+            double min = arr[0];
+            for (double i : arr) {
+                if (min > i) min = i;
+            }
+
+            return min;
 
         }
     }
+
+    static void sort(double[] array) {
+        boolean swap;
+        int last = array.length - 1;
+        do {
+            swap = false;
+            for (int i = 0; i < last; i++) {
+                if (array[i] > array[i + 1]) {
+                    double buffer = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = buffer;
+                    swap = true;
+                }
+            }
+            last--;
+
+        } while (swap);
+        printArray(array);
+    }
+
+    static private void printArray(double[] array) {
+        for (double i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
+
--- src/by/it/vesniachok/jd01_06/TaskA2.java
+++ src/by/it/vesniachok/jd01_06/TaskA2.java
@@ -1,27 +1,25 @@
 package by.it.vesniachok.jd01_06;
 
+
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskA2 {
-    static private String[] words = {};
-    static private int[] counts = {};
+    private static String[] words = new String[0];
+    private static int[] counts = {};
 
     public static void main(String[] args) {
-
-        System.out.println(Poem.text);
-        Pattern pattern = Pattern.compile("[а-я-ЯёЁ]{4,}");
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
         Matcher matcher = pattern.matcher(Poem.text);
         while (matcher.find()) {
             String word = matcher.group();
             process(word);
-
         }
         for (int i = 0; i < words.length; i++) {
-            System.out.printf("%s=%d\n", words[i], counts[i]);
-
+            System.out.printf("%s=%d\n",words[i],counts[i]);
         }
+
     }
 
     private static void process(String word) {
@@ -30,20 +28,14 @@ public class TaskA2 {
                 counts[i]++;
                 return;
             }
-
         }
-        Arrays.copyOf(words, words.length + 1);
-        Arrays.copyOf(counts, counts.length + 1);
+        words = Arrays.copyOf(words, words.length + 1);
         words[words.length - 1] = word;
+        counts = Arrays.copyOf(counts, counts.length + 1);
         counts[counts.length - 1] = 1;
-
-
-    }
-    private static void printResults() {
-        for (int i = 0; i < words.length; i++) {
-            System.out.printf("%s=%d%n", words[i], counts[i]);
-        }
     }
 }
+
+
 
 
