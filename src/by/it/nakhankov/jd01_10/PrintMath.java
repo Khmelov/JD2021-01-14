2e61412f333f643d23a9e8c0f597f6041b6854fe
aliaksei <alekseinakhankov@gmail.com>
2021-02-08 10:16:10 +0300

    jd01_10

---
+++ src/by/it/nakhankov/jd01_10/PrintMath.java
@@ -0,0 +1,16 @@
+package by.it.nakhankov.jd01_10;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class PrintMath {
+    public static void main(String[] args) {
+        Class<Math> structMath = Math.class;
+        Method[] methods = structMath.getDeclaredMethods();
+        for (Method method : methods) {
+           if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
+               System.out.println(method);
+        }
+
+    }
+}
