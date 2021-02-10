766e2a3d55d8af30dd22a913cd812809ae8be0fd
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-02-10 19:08:24 +0300

    jd09

--- src/by/it/petrov/jd01_11/Runner.java
+++ src/by/it/petrov/jd01_11/Runner.java
@@ -1,9 +1,6 @@
 package by.it.petrov.jd01_11;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Runner {
 
@@ -14,20 +11,18 @@ public class Runner {
 
         List<Integer> expList = new ArrayList<>();
 
+        expList.add(null);expList.add(5);expList.add(7);expList.add(5);expList.add(5);expList.add(777);
 
-        expList.add(5);expList.add(3);expList.add(2);expList.add(5);expList.add(0);
-
-        mySet.add(1);mySet.add(2);mySet.add(3);mySet.add(4);mySet.add(5);
+        mySet.add(1);mySet.add(null);mySet.add(3);mySet.add(4);mySet.add(5);
         mySet.add(6);mySet.add(7);mySet.add(8);mySet.add(9);mySet.add(10);
 
-        mySet.toSet(expList);
-
-
+        System.out.println(mySet.contains(5));
+        mySet.removeAll(expList);
+        System.out.println(mySet);
 
         arSet.add(1);arSet.add(2);arSet.add(3);arSet.add(4);arSet.add(5);
         arSet.add(6);arSet.add(7);arSet.add(8);arSet.add(9);arSet.add(10);
         System.out.println("My Set is: " + mySet.toString() + "\nAr Set is: " + arSet.toString());
-        System.out.println(mySet.contains(5));
 
     }
 }
