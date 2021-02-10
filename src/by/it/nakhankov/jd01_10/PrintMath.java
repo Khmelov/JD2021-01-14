78087c32a05e25b53b3c0a802a67a71014132fe4
aliaksei <alekseinakhankov@gmail.com>
2021-02-10 16:41:54 +0300

    jd01_10-jd01-11

--- src/by/it/nakhankov/jd01_10/PrintMath.java
+++ src/by/it/nakhankov/jd01_10/PrintMath.java
@@ -1,16 +1,37 @@
 package by.it.nakhankov.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class PrintMath {
     public static void main(String[] args) {
-        Class<Math> structMath = Math.class;
-        Method[] methods = structMath.getDeclaredMethods();
-        for (Method method : methods) {
-           if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
-               System.out.println(method);
-        }
+        printMethods();
+        printField();
 
+    }
+
+    public static void printMethods() {
+        Method[] declaredMethods = Math.class.getDeclaredMethods();
+
+        for (Method method : declaredMethods) {
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
+                StringBuilder str = new StringBuilder(method.toString());
+                str.replace(str.lastIndexOf(" "), str.lastIndexOf(".") + 1, " ");
+                System.out.println(str);
+            }
+        }
+    }
+
+    public static void printField() {
+        Field[] declaredFields = Math.class.getDeclaredFields();
+
+        for (Field field : declaredFields) {
+            if ((field.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
+                StringBuilder str = new StringBuilder(field.toString());
+                str.replace(str.lastIndexOf(" "), str.lastIndexOf(".") + 1, " ");
+                System.out.println(str);
+            }
+        }
     }
 }
