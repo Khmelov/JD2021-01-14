f0b5553600adf6c521a53a4efddd8236f2f56423
Zevalich <afina.vi.s@gmail.com>
2021-02-17 00:21:30 +0300

    ..

--- src/by/it/zevalich/jd01_12/TaskB1.java
+++ src/by/it/zevalich/jd01_12/TaskB1.java
@@ -1,25 +1,30 @@
 package by.it.zevalich.jd01_12;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class TaskB1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int count = 0;
-        String str;
-        List<String> list = new ArrayList<>() ;
-        HashMap<String, Integer> map = new HashMap<>();
-        while (true){
-            str = scanner.nextLine();
-            count++;
-            list.add(str);
-            if(str.equals("end") && count == 100){
-                break;
-            }
+        Map<String,Integer> map = new HashMap<>();
+        List<String> string = new ArrayList<>();
+        String s;
+        while (!(s=scanner.nextLine()).equals("end")){
+            String str = s.replaceAll("[^a-zA-Z’']", "");
+            string.add(str);
+        }
+        for (int i = 0; i < string.size(); i++) {
+            String str = string.get(i);
+                if(!(map.containsKey(str))){
+                    map.put(str,1);
+                }
+                else{
+                    map.put(str,map.get(str)+1);
+                }
         }
 
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey()+ "=" + entry.getValue());
+        }
+        }
     }
-}
+
