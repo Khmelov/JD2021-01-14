f277d6e11cd9f12ba18bb7c8b2cbf7929f6f0bce
Sergei Voitenkov <Voitenkov.Sergei@gmail.com>
2021-02-18 02:42:52 +0300

    14 theme solved

--- src/by/it/voitenkov/jd01_15/TaskB.java
+++ src/by/it/voitenkov/jd01_15/TaskB.java
@@ -10,75 +10,106 @@ import java.nio.file.Paths;
 
 public class TaskB {
 
-    public static void main(String[] args) {
-        readFileJava(); //dsad;
-        //dasdas
-        /*adds
-         */
-        /*asdasd
+    // Комментарий строчный 2
+    int a3 = 1;/*
+    Комментарий многострочный:
+    строка 11
+    строка 12
+*/
+    int a2 = 1;
+    int a = 1;/*
+    Комментарий многострочный:
+    строка 21
+    строка 22
+    строка 23
+    строка 24
+    строка 25
+     */
+    int a1 = 1;
 
-         */
+    // Комментарий строчный 2
+    public static void main(String[] args) {
+        readFileJava();
     }
 
     static void readFileJava() {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuffer = new StringBuilder();
         File file = getFile(TaskB.class, "TaskB.java");
         Path path = Paths.get(file.toString());
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String line;
+            BufferedReader inputStreamReader = new BufferedReader(new FileReader(file));
+            String str;
             boolean flagComment = false;
             boolean flagJavaDOC = false;
             boolean flag = false;
 
-            boolean multiLine = false; // KU
-            //Main Loop
-            while ((line = bufferedReader.readLine()) != null) {
-                //line = line + "       ";
-                boolean dropline = false;
-                char[] chars = line.toCharArray();
+            while ((str = inputStreamReader.readLine()) != null) {
+
+                char[] chars = str.toCharArray();
                 for (int i = 0; i < chars.length; i++) {
-                    if (!multiLine){
-                        //Detect JavaDoc Comments
-                        if (chars[i] == '/' && chars[i + 1] == '*' && chars[i+2] == '*'){
-                            multiLine = true;
-                            dropline = true;
+                    if (chars[i] == '/' && chars[i + 1] == '/') {
+                        flag = false;
+                        break;
+                    } else if (str.length() - i > 2 && chars[i] == '/' && chars[i + 1] == '*' && chars[i + 2] == '*' || flagJavaDOC) {
+                        if (!flagJavaDOC) {
+                            flagJavaDOC = true;
+                            break;
                         }
-                        //Detect Multi line Comments
-                        else if (chars[i] == '/' && chars[i + 1] == '*') {
-                            multiLine = true;
-                            dropline = true;
-                        }
-                        //Detect Single Comments
-                        else if (chars[i] == '/' && chars[i + 1] == '/') {
-                            if (i == 0) {
-                                dropline = true;
+                        while (flagJavaDOC) {
+                            if (chars[i] == '*' && chars[i + 1] == '/') {
+                                flagJavaDOC = false;
+                                i += 2;
+                                if (i == str.length() - 1) {
+                                    stringBuffer.append(str.substring(i)).append("\n");
+                                } else {
+                                    stringBuffer.append(str.substring(i));
+                                }
                             } else {
-                                line = line.substring(0, i);
+                                i++;
+                            }
+                            if (i == chars.length - 1) {
+                                break;
                             }
                         }
-                    }else {
-                        if (chars[i] == '*'&& chars[i+1] == '/'){
-                            multiLine = false;
+                    } else if (chars[i] == '/' && chars[i + 1] == '*' || flagComment) {
+                        flagComment = true;
+                        while (flagComment) {
+                            if (chars[i] == '*' && chars[i + 1] == '/') {
+                                flagComment = false;
+                                i += 2;
+                                if (i == str.length() - 1) {
+                                    stringBuffer.append(str.substring(i));
+                                } else {
+                                    stringBuffer.append(str.substring(i)).append("\n");
+                                }
+
+                            } else {
+                                i++;
+                            }
+
+                            if (i == str.length() - 1) {
+                                break;
+                            }
+
                         }
-                        dropline = true;
+                    } else {
+                        stringBuffer.append((char) chars[i]);
+                        flag = true;
                     }
-
                 }
 
-                if (!dropline) {
-                    stringBuilder.append(line).append(System.getProperty("line.separator"));
+                if (flag) {
+                    stringBuffer.append("\n");
+                    flag = false;
                 }
-
             }
-            writeFile(stringBuilder);
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (
+                IOException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
         }
-        System.out.println(stringBuilder.toString());
+        System.out.println(stringBuffer.toString());
 
-        writeFile(stringBuilder);
+        writeFile(stringBuffer);
 
     }
 
