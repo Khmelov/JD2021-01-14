021d5fd790fb684aecdc84a0dddbe3cf7513a9db
Pavel Lapushkin <zerocoolik@mail.ru>
2021-02-19 17:28:34 +0300

    02_01 ABC

---
+++ src/by/it/lapushkin/jd02_01/Helper.java
@@ -0,0 +1,30 @@
+package by.it.lapushkin.jd02_01;
+
+import java.util.Random;
+
+public class Helper {
+    private final static int K_SPEED = 10;
+    private final static Random RND = new Random();
+
+    private Helper() {
+    }
+
+    public static void timeout(int millis) {
+        try {
+            Thread.sleep(millis / K_SPEED);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int random(int start, int end) {
+        return RND.nextInt(end - start + 1) + start;
+    }
+
+    public static int random(int max) {
+        return random(0, max);
+    }
+    public static int random(int max,Boolean positive){
+        return random(1, max);
+    }
+}
