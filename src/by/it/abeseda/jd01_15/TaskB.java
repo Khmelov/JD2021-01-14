5a9853fe0fd06769ba13478b51838aba27fe9352
AlexandraBeseda <s.ani.aa862@gmail.com>
2021-02-24 11:10:15 +0300

    jd01_15 Task B, перенесла меод в утил, все работает, хитрсоть бла в разнесении методов по отдельным классам

--- src/by/it/abeseda/jd01_15/TaskB.java
+++ src/by/it/abeseda/jd01_15/TaskB.java
@@ -11,50 +11,32 @@ import java.io.*;
 
 public class TaskB {
 
-    private static final String dir = System.getProperty("user.dir") + File.separator +
-            "src" + File.separator +
-            TaskB.class.getPackageName().replace(".", File.separator)+File.separator;
+//    private static final String dir = System.getProperty("user.dir") + File.separator +
+//            "src" + File.separator +
+//            TaskB.class.getPackageName().replace(".", File.separator)+File.separator;
 
-    private static final String fileToRead=dir+"TaskB.java";
-    private static final String fileToWrite=dir+"TaskB.txt";
+//    private static final String fileToRead=dir+"TaskB.java";
+//    private static final String fileToWrite=dir+"TaskB.txt";
+
+    static File file1 = Util.getFile(TaskB.class, "TaskB.java");
+    static File file2 = Util.getFile(TaskB.class, "TaskB.txt");
 
     public static void main(String[] args) {//медведи не летают
-        StringBuilder text=readWholeText(fileToRead);
+        StringBuilder text=readWholeText(file1);
 /*рыбп
 */
-        while (Util.isTextContainsJavaDocLineComment(text)){
-            int begin=text.indexOf("/**");//вот это прога удалила, но всеок
-            int end=text.indexOf("*/", begin);
-            if(end==-1 || begin==-1){
-                break;
-            }else{
-            text.replace(begin, end+2, "");}
-        }
-        while (Util.isTextContainsSingleLineComment(text)){
-            int begin=text.indexOf("/"+"/");
-            int end=text.indexOf("\r", begin);
-            if(end==-1||begin==-1){
-                break;
-            }else{
-                text.replace(begin, end, "");}
-        }
-        while (Util.isTextContainsMultiLineComment(text)){
-            int begin=text.indexOf("/*");
-            int end=text.indexOf("*/", begin);
-            if(end==-1||begin==-1){
-                break;
-            }else{
-            text.replace(begin, end+2, "");}
-        }
+        Util.textWithoutComments(text);
 /*
 hjhvfgfjfjfjfjffvfj
 jhjhvjhvjhvjhvjv
  */
-        writeWholeText(text,fileToWrite);
+        writeWholeText(text,file2);
         System.out.println(text);//медведи не летают
     }
 
-    private static void writeWholeText(StringBuilder text, String fileToWrite) {
+
+
+    private static void writeWholeText(StringBuilder text, File fileToWrite) {
         try (PrintWriter out=new PrintWriter(new FileWriter(fileToWrite))){
             out.print(text);
         } catch (IOException e) {
@@ -62,7 +44,7 @@ jhjhvjhvjhvjhvjv
         }
     }
 
-    private static StringBuilder readWholeText(String fileToRead) {
+    private static StringBuilder readWholeText(File fileToRead) {
         StringBuilder sb = new StringBuilder();
         try (FileReader fileReader = new FileReader(fileToRead)) {
             int c;
--- src/by/it/abeseda/jd01_15/Util.java
+++ src/by/it/abeseda/jd01_15/Util.java
@@ -5,13 +5,10 @@ import java.io.File;
 public class Util {
     static File getFile(Class<?> aClass, String fileName) {
         String packageName = aClass.getPackageName();
-        String dir = System.getProperty("user.dir") + File.separator +
-                "src" + File.separator +
+        String dir = System.getProperty("user.dir") + File.separator + "src" + File.separator +
                 packageName.replace(".", File.separator);
         return new File(dir, fileName);
     }
-
-
 
     //для второго задания
     static boolean isTextContainsSingleLineComment(StringBuilder text) {
@@ -30,5 +27,30 @@ public class Util {
         return index!=-1;
     }
 
-
+    static void textWithoutComments(StringBuilder text) {
+        while (Util.isTextContainsJavaDocLineComment(text)){
+            int begin= text.indexOf("/**");//вот это прога удалила, но всеок
+            int end= text.indexOf("*/", begin);
+            if(end==-1 || begin==-1){
+                break;
+            }else{
+                text.replace(begin, end+2, "");}
+        }
+        while (Util.isTextContainsSingleLineComment(text)){
+            int begin= text.indexOf("/"+"/");
+            int end= text.indexOf("\r", begin);
+            if(end==-1||begin==-1){
+                break;
+            }else{
+                text.replace(begin, end, "");}
+        }
+        while (Util.isTextContainsMultiLineComment(text)){
+            int begin= text.indexOf("/*");
+            int end= text.indexOf("*/", begin);
+            if(end==-1||begin==-1){
+                break;
+            }else{
+                text.replace(begin, end+2, "");}
+        }
+    }
 }
