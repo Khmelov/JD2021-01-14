75f8971de34b2aa99a7f4c440e3055f6430e1f50
Alexander Khmelov <akhmelev@gmail.com>
2021-02-27 16:30:42 +0300

    jd02_05 add path

--- src/by/it/_classwork_/jd02_05/txt/convertor.bat
+++ src/by/it/_classwork_/jd02_05/txt/convertor.bat
@@ -1,3 +1,4 @@
+set path=E:\Java\jdk-8\bin
 native2ascii -encoding UTF-8 language.txt ../lang/language.properties
 native2ascii -encoding UTF-8 language_en.txt ../lang/language_en.properties
 native2ascii -encoding UTF-8 language_ru_RU.txt ../lang/language_ru_RU.properties
