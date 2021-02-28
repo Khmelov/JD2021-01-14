848e3ecb8a304323149fdfc5b1001ad6ee5ae0d5
Pavel Lapushkin <zerocoolik@mail.ru>
2021-03-01 01:00:01 +0300

    JD02_03 ABC

--- src/by/it/lapushkin/jd02_03/Context.java
+++
@@ -1,8 +0,0 @@
-package by.it.lapushkin.jd02_03;
-
-public class Context {
-    private Monitoring monitoring;
-    private Manager manager;
-    private Score score;
-
-}
--- src/by/it/lapushkin/jd02_03/Runner.java
+++
@@ -1,7 +0,0 @@
-package by.it.lapushkin.jd02_03;
-
-public class Runner {
-    public static void main(String[] args) {
-        new Manager("Main manager").start();
-    }
-}
