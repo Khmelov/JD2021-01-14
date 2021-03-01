566689be143c0b4c652e3ff6a54bc5ccf9b17f92
AlexandraBeseda <s.ani.aa862@gmail.com>
2021-03-01 19:04:43 +0300

    calculator

--- src/by/it/abeseda/calc/Patterns.java
+++ src/by/it/abeseda/calc/Patterns.java
@@ -5,16 +5,13 @@ public class Patterns {
     //счітаем первым делом с скобках
     static final String OPERATION_FIRST="[(]([^()]+)[)]";
 
-    static final String OPERATION="(?<=[^-+*/=,{])[-+*/=]";//паттерн для поиска вида операции
+    static final String OPERATION="(?<=[^-+*/=,{])[-+*/=]";
+    //паттерн для поиска вида операции
     //исключаем знак перед числом
     // A=-2*{-4,6,-8}--3+-6
-    static final String SCALAR="-?[0-9]+\\.?[0-9]*";//паттерн для поиска скалярной величины
- //   static final String SCALAR="-?[0-9]+(.[0-9]+)?";//паттерн для поиска скалярной величины
+    static final String SCALAR="-?[0-9]+(.[0-9]+)?";
 
- //   static final String VECTOR= "\\{" + SCALAR + "(," + SCALAR + ")*}";;//паттерн для поиска вектора
-    static final String VECTOR= "[{]((" + SCALAR + ")(,?)(\\s?))+}";//паттерн для поиска вектора
+    static final String VECTOR= "\\{" + SCALAR + "(," + SCALAR + ")*}";
 
-
-// static final String MATRIX="\\{" + VECTOR + "," + VECTOR + "+ }";
-    static final String MATRIX="[{](" + VECTOR + ",?\\s?)+}";
+    static final String MATRIX="\\{" + VECTOR + "(," + VECTOR + ")*}";
 }
