f0cd8aa278c591ed22fec671941313ed401a4d25
it.academy.vs@gmail.com <it.academy.vs@gmail.com>
2021-03-02 12:29:13 +0300

    classwork jd02_06

---
+++ src/by/it/_classwork_/jd02_06/Logger.java
@@ -0,0 +1,41 @@
+package by.it._classwork_.jd02_06;
+
+import java.io.*;
+
+class Logger {
+
+    private File logFile;
+
+    private static volatile Logger instance;
+
+    private Logger() {
+        logFile = Util.getFile(Logger.class, "log.txt");
+    }
+
+    public static Logger getLogger() {
+        Logger local = instance;
+        if (local == null) {
+            synchronized (Logger.class) {
+                local =instance;
+                if (local == null) {
+                    local = new Logger();
+                    instance = local;
+                }
+            }
+        }
+        return local;
+    }
+
+
+    synchronized void log(String logText) {
+        try (
+                PrintWriter logWriter = new PrintWriter(
+                        new FileWriter(logFile, true)
+                )
+        ) {
+            logWriter.println(logText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/by/it/_classwork_/jd02_06/Util.java
@@ -0,0 +1,16 @@
+package by.it._classwork_.jd02_06;
+
+import java.io.File;
+
+public class Util {
+    public static File getFile(Class<?> aClass, String fileName) {
+        String packageName = aClass.getPackageName();
+        String root = System.getProperty("user.dir");
+        String dir = root +
+                File.separator +
+                "src" +
+                File.separator +
+                packageName.replace(".", File.separator);
+        return new File(dir, fileName);
+    }
+}
