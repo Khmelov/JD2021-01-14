dda08767b14fb4b7c1f7d915f3ef578b85a56c0d
AlexandraBeseda <s.ani.aa862@gmail.com>
2021-03-03 18:50:15 +0300

    last task

--- src/by/it/abeseda/calc_jd02_06_07/Printer.java
+++
@@ -1,9 +0,0 @@
-package by.it.abeseda.calc_jd02_06_07;
-
-public class Printer {
-    void print(Var var){
-        if (var!=null){
-            System.out.println(var);
-        }
-    }
-}
--- src/by/it/abeseda/calc_jd02_06_07/VarCreator.java
+++
@@ -1,5 +0,0 @@
-package by.it.abeseda.calc_jd02_06_07;
-
-public class VarCreator {
-
-}
